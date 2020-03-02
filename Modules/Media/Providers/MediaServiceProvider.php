f0c8466698886b32746eff762489bcfc86052912
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-02 20:53:38 +0500

    media module fix2

---
+++ Modules/Media/Providers/MediaServiceProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\Media\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class MediaServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->registerFactories();
+        $this->loadMigrationsFrom(module_path('Media', 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            module_path('Media', 'Config/config.php') => config_path('media.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path('Media', 'Config/config.php'), 'media'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = resource_path('views/modules/media');
+
+        $sourcePath = module_path('Media', 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ],'views');
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/media';
+        }, \Config::get('view.paths')), [$sourcePath]), 'media');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = resource_path('lang/modules/media');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'media');
+        } else {
+            $this->loadTranslationsFrom(module_path('Media', 'Resources/lang'), 'media');
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('Media', 'Database/factories'));
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
