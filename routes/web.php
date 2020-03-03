73ff5acda21a30190560af0d066609d445aac6da
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-04 01:36:40 +0500

    routing fixed

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('layouts/app');
+});
+
+Route::get('/app', function () {
+    return view('layouts/app');
+});
 //
 //Auth::routes();
 //
