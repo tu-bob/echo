aebf2a07015274d616b3e192744cd9504b60d36a
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-18 23:46:47 +0500

    save original file name

--- Modules/Media/Database/Migrations/2020_03_07_083002_create_audio_files_table.php
+++ Modules/Media/Database/Migrations/2020_03_07_083002_create_audio_files_table.php
@@ -16,7 +16,8 @@ class CreateAudioFilesTable extends Migration
         Schema::create('audio_files', function (Blueprint $table) {
             $table->char('id', 26)->primary();
             $table->string('path');
-            $table->string('filename');
+            $table->char('filename', 40);
+            $table->char('original_filename', 40);
             $table->string('mime_type');
             $table->char('extension', 10);
             $table->string('hash');
