43d1e751ba50e77b1366c43883a749f93e2cb762
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-20 01:19:59 +0500

    filename length increased. minor fix

--- Modules/Media/Database/Migrations/2020_03_07_083002_create_audio_files_table.php
+++ Modules/Media/Database/Migrations/2020_03_07_083002_create_audio_files_table.php
@@ -16,8 +16,8 @@ class CreateAudioFilesTable extends Migration
         Schema::create('audio_files', function (Blueprint $table) {
             $table->char('id', 26)->primary();
             $table->string('path');
-            $table->char('filename', 40);
-            $table->char('original_filename', 40);
+            $table->char('filename', 50);
+            $table->char('original_filename', 255);
             $table->string('mime_type');
             $table->char('extension', 10);
             $table->string('hash');
