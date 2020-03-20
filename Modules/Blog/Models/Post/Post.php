06750ee7f22cb504159a77beb69220f825808d30
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-21 02:34:48 +0500

    edit post

--- Modules/Blog/Models/Post/Post.php
+++ Modules/Blog/Models/Post/Post.php
@@ -4,11 +4,16 @@
 namespace Modules\Blog\Models\Post;
 
 
+use Modules\Blog\Models\Author\Author;
 use Modules\Media\Models\Image\ImageFile;
 use Modules\Shared\Models\BaseModel;
 
 class Post extends BaseModel
 {
+    public function author(){
+        return $this->belongsTo(Author::class);
+    }
+
     public function previewImage()
     {
         return $this->belongsTo(ImageFile::class);
