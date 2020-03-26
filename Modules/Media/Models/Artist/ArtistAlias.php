eaf4a8e58fdf62fb8400aa40df45af5927cabd84
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-26 23:59:06 +0500

    hidden fields in Song.php and ArtistAlias.php

--- Modules/Media/Models/Artist/ArtistAlias.php
+++ Modules/Media/Models/Artist/ArtistAlias.php
@@ -7,5 +7,12 @@ use Modules\Shared\Models\BaseModel;
 
 class ArtistAlias extends BaseModel
 {
-
+    protected $hidden = [ 
+        'created_at',
+        'created_by_id',
+        'deleted_at',
+        'deleted_by_id',
+        'updated_at',
+        'updated_by_id'
+    ];
 }
