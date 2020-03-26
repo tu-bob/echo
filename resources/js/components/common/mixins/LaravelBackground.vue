eb552d0380df92998a3cdffdb42537dad556db3b
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-26 12:25:52 +0500

    mixin for Laravel Background

---
+++ resources/js/components/common/mixins/LaravelBackground.vue
@@ -0,0 +1,13 @@
+<script>
+    import {removeLaravelBackground, setLaravelBackground} from "../../../main";
+
+    export default {
+        name: "LaravelBackground",
+        mounted() {
+            setLaravelBackground()
+        },
+        beforeDestroy() {
+            removeLaravelBackground()
+        }
+    }
+</script>
