75a31c866bf543322bfe36c46c75bdf236cca9b4
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-26 14:01:49 +0500

    songs carousel

---
+++ resources/js/components/public/home/overview/SongCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container pb-3">
+        <div class="row w-100" style="color:white">
+            <b-img class="bg-laravel thumb"
+                   :src="thumbnail"
+                   rounded
+            ></b-img>
+            <div class="info-wrapper">
+                <h6 class="mb-0">The separation</h6>
+                <span class="text-muted">Rachel Platten</span>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "SongCard",
+        props: {
+            thumbnail: {
+                type: String,
+                default: 'https://picsum.photos/1024/400/?image=41'
+            }
+        }
+    }
+
+</script>
+<style scoped>
+    .thumb {
+        object-fit: cover;
+        width: 50px !important;
+        height: 50px;
+    }
+
+    .info-wrapper {
+        padding-left: 15px;
+        padding-top: 7px;
+    }
+</style>
---
+++ resources/js/components/public/home/overview/SongsСarousel.vue
@@ -0,0 +1,42 @@
+<template>
+    <carousel :nav="false" :dots="false" :responsive="{0:{items:1},540:{items:2},720:{items:3},1200:{items:4}}">
+        <div>
+            <song-card></song-card>
+            <song-card></song-card>
+            <song-card></song-card>
+            <song-card></song-card>
+        </div>
+        <div>
+            <song-card></song-card>
+            <song-card></song-card>
+            <song-card></song-card>
+            <song-card></song-card>
+        </div>
+        <div>
+            <song-card></song-card>
+            <song-card></song-card>
+            <song-card></song-card>
+            <song-card></song-card>
+        </div>
+        <div>
+            <song-card></song-card>
+            <song-card></song-card>
+            <song-card></song-card>
+            <song-card></song-card>
+        </div>
+    </carousel>
+</template>
+
+<script>
+    import SongCard from "./SongCard";
+    import Carousel from 'vue-owl-carousel'
+
+    export default {
+        name: "SongsCarousel",
+        components: {SongCard, Carousel}
+    }
+</script>
+
+<style scoped>
+
+</style>
