d18ac11f0e3cc4ed16401e9552758564dba76a55
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-04-03 14:11:41 +0500

    AlbumsList.vue and VideosList.vue pagination

--- resources/js/components/common/music/album/AlbumsList.vue
+++ resources/js/components/common/music/album/AlbumsList.vue
@@ -3,6 +3,15 @@
         <album-card class="col-12 col-md-6 col-lg-4 col-xl-3 mb-5"
                     v-for="album in albums" :key="album.id"
                     :album="album"></album-card>
+
+        <pagination v-if="pagination"
+                    ref="albums-list-pagination"
+                    flow
+                    :pagination="pagination"
+                    class="mt-5"
+                    @pageChanged="fetchAlbums"
+                    v-observe-visibility="paginationIntersObj">
+        </pagination>
     </div>
 </template>
 
@@ -17,18 +26,37 @@
         },
         data() {
             return {
-                paginator: null,
-                albums: []
+                pagination: null,
+                albums: [],
+                isFetchingAlbums: false,
+                paginationIntersObj: {
+                    callback: this.paginationVisibilityChanged,
+                    intersection: {
+                        threshold: 0.3,
+                        rootMargin: '200px',
+                        throttle: 100
+                    }
+                }
             }
         },
         methods: {
             fetchAlbums(page = 1) {
+                this.isFetchingAlbums = true;
                 fetchAlbums({order: 'latest', songs: 1, page: page, paginate: 15})
                     .then(response => {
-                        this.paginator = response;
+                        this.pagination = response;
                         this.albums.push(...response.data)
                     })
                     .catch()
+                    .then(_ => {
+                        this.$refs['albums-list-pagination'].loaded();
+                        this.isFetchingAlbums = false;
+                    })
+            },
+            paginationVisibilityChanged(isVisible, entry) {
+                let btn = entry.target.querySelector('button');
+                if (isVisible && !this.isFetchingAlbums && btn)
+                    btn.click()
             }
         },
         components: {AlbumCard}
