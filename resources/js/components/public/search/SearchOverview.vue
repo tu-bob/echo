6b01127807f0459e8108694af2ab9154dc2dc0df
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-04-05 23:12:10 +0500

    search overview

--- resources/js/components/public/search/SearchOverview.vue
+++ resources/js/components/public/search/SearchOverview.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-input-group prepend="Поиск" class="mt-3">
+        <b-input-group prepend="Поиск" class="mt-3 mb-5">
             <input @keydown.enter="search"
                    type="text"
                    class="form-control bg-dark text-white border-secondary"
@@ -10,14 +10,37 @@
             </b-input-group-append>
         </b-input-group>
 
+        <section class="section" v-if="songs.length > 0">
+            <h2 class="title">Песни</h2>
+            <div class="row">
+                <songs-list :playlist="songs.slice(0, Math.ceil(songs.length/2))" no-fetch
+                            class="col-md-6"></songs-list>
+                <songs-list :playlist="songs.slice(Math.ceil(songs.length/2))" no-fetch class="col-md-6"></songs-list>
+            </div>
+        </section>
+
+        <section class="section" v-if="albums.length > 0">
+            <h2 class="title">Альбомы</h2>
+            <albums-list :provided-albums="albums" no-fetch></albums-list>
+        </section>
+
+        <section class="section" v-if="videos.length > 0">
+            <h2 class="title">Видео</h2>
+            <videos-list :provided-videos="videos" no-fetch></videos-list>
+        </section>
     </div>
 </template>
 
 <script>
     import {search} from "../../../api/mediaApi";
+    import AlbumsList from "../../common/music/album/AlbumsList";
+    import SongsList from "../../common/music/song/SongsList";
+    import VideosList from "../../common/video/VideosList";
+
 
     export default {
         name: "SearchOverview",
+        components: {VideosList, AlbumsList, SongsList},
         data() {
             return {
                 query: '',
@@ -31,8 +54,8 @@
                 search(this.query)
                     .then(response => {
                         this.songs = response.songs;
-                        this.videos = response.videos;
                         this.albums = response.albums;
+                        this.videos = response.videos;
                     })
                     .catch()
             }
