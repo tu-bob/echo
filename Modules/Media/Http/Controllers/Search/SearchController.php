0b9afede4a014d8132dd9033c1316fb3bed0ec5e
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-04-05 22:29:17 +0500

    search method

---
+++ Modules/Media/Http/Controllers/Search/SearchController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Modules\Media\Http\Controllers\Search;
+
+
+use Modules\Media\Models\Music\MusicAlbum;
+use Modules\Media\Models\Music\Song;
+use Modules\Media\Models\Video\Video;
+use Modules\Shared\Http\Controllers\BaseController;
+
+class SearchController extends BaseController
+{
+    public function search($needle)
+    {
+        $songs = Song::where('title', 'like', "%{$needle}%")
+            ->orWhereHas('artistAliases', function ($query) use ($needle) {
+                $query->where('name', 'like', "%{$needle}%");
+            })->limit(10)->get();
+//        $artist = ArtistAlias::where('name', 'like', "%{$query}%")->limit(10)->get();
+        $albums = MusicAlbum::where('title', 'like', "%{$needle}%")
+            ->orWhereHas('artistAliases', function ($query) use ($needle) {
+                $query->where('name', 'like', "%{$needle}%");
+            })->limit(10)->get();
+
+        $videos = Video::where('title', 'like', "%{$needle}%")->limit(10)->get();
+
+        return response()->json([
+            'songs' => $songs,
+            'albums' => $albums,
+            'videos' => $videos
+        ], 200);
+    }
+}
