62d4c90e9d3f266018c05eb79421796fb8ef8d6b
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-04-05 17:28:05 +0500

    overview update

--- resources/js/components/public/home/overview/Overview.vue
+++ resources/js/components/public/home/overview/Overview.vue
@@ -1,8 +1,20 @@
 <template>
     <div>
-        <songs-list :playlist="songs" no-fetch></songs-list>
-        <hr>
-        <albums-list></albums-list>
+        <section class="mb-5">
+            <h2 class="mb-5">Музыка достойная вас</h2>
+            <div class="row">
+                <songs-list :playlist="songs.slice(0, Math.ceil(songs.length/2))" no-fetch class="col-md-6"></songs-list>
+                <songs-list :playlist="songs.slice(Math.ceil(songs.length/2))" no-fetch class="col-md-6"></songs-list>
+            </div>
+        </section>
+        <section class="mb-5">
+            <h2 class="mb-5">Прослушайте эти альбомы</h2>
+            <albums-list></albums-list>
+        </section>
+        <section class="mb-5">
+            <h2 class="mb-5">Лучшие клипы</h2>
+            <videos-list></videos-list>
+        </section>
     </div>
 
 </template>
@@ -11,6 +23,7 @@
     import AlbumsList from "../../../common/music/album/AlbumsList";
     import {fetchSongs} from "../../../../api/mediaApi";
     import SongsList from "../../../common/music/song/SongsList";
+    import VideosList from "../../../common/video/VideosList";
 
     export default {
         name: "Overview",
@@ -22,10 +35,10 @@
                 songs: []
             }
         },
-        components: {SongsList, AlbumsList},
+        components: {VideosList, SongsList, AlbumsList},
         methods: {
             fetchTopSongs() {
-                fetchSongs({order: 'latest,play_count,download_count', limit: 5})
+                fetchSongs({order: 'latest,play_count,download_count', limit: 8})
                     .then(songs => this.songs = songs)
                     .catch()
             }
@@ -33,3 +46,10 @@
     }
 
 </script>
+
+<style scoped>
+    h2 {
+        color: #6c757d;
+        font-size: 1.4rem;
+    }
+</style>
