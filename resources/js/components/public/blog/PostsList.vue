007b2be7f4075093d5fbdfd7b228508837c47867
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-04-06 12:46:38 +0500

    posts list

---
+++ resources/js/components/public/blog/PostsList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row">
+        <post-preview class="col-sm-6 col-md-4 col-lg-3 col-xl-2" v-for="post in posts" :post="post" :key="post.id"></post-preview>
+    </div>
+</template>
+
+<script>
+    import PostPreview from "../../common/blog/PostPreview";
+    export default {
+        name: "PostsList",
+        components: {PostPreview},
+        mounted() {
+            if (this.providedPosts)
+                this.posts = this.providedPosts;
+        },
+        props: {
+            providedPosts: {
+                type: Array
+            },
+            noFetch: {
+                type: Boolean
+            }
+        },
+        data() {
+            return {
+                posts: []
+            }
+        },
+        watch: {
+            providedPosts() {
+                this.posts = this.providedPosts;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/public/home/overview/Overview.vue
+++ resources/js/components/public/home/overview/Overview.vue
@@ -41,6 +41,17 @@
             </div>
             <videos-list :provided-videos="videos" no-fetch></videos-list>
         </section>
+        <section class="section col-12">
+            <div class="row">
+                <h2 class="col-8 title">Свежие статьи</h2>
+                <div class="col-4 text-right">
+                    <router-link :to="{name:'blog'}" class="ml-auto btn-text-white text-decoration-none">
+                        <span>больше статей</span>
+                    </router-link>
+                </div>
+            </div>
+            <posts-list :provided-posts="posts" no-fetch></posts-list>
+        </section>
     </div>
 
 </template>
@@ -50,6 +61,8 @@
     import {fetchAlbums, fetchSongs, fetchVideos} from "../../../../api/mediaApi";
     import SongsList from "../../../common/music/song/SongsList";
     import VideosList from "../../../common/video/VideosList";
+    import PostsList from "../../blog/PostsList";
+    import {fetchPosts} from "../../../../api/blogApi";
 
     export default {
         name: "Overview",
@@ -57,15 +70,17 @@
             this.fetchTopSongs();
             this.fetchTopAlbums();
             this.fetchTopVideos();
+            this.fetchTopPosts();
         },
         data() {
             return {
                 songs: [],
                 albums: [],
-                videos: []
+                videos: [],
+                posts: []
             }
         },
-        components: {VideosList, SongsList, AlbumsList},
+        components: {PostsList, VideosList, SongsList, AlbumsList},
         methods: {
             fetchTopSongs() {
                 fetchSongs({order: 'play_count:desc,download_count:desc', limit: 8})
@@ -80,6 +95,11 @@
             fetchTopVideos() {
                 fetchVideos({order: 'play_count:desc,download_count:desc', limit: 8})
                     .then(videos => this.videos = videos)
+                    .catch()
+            },
+            fetchTopPosts() {
+                fetchPosts({order: 'view_count:desc', limit: 4})
+                    .then(posts => this.posts = posts)
                     .catch()
             },
             updatePlaylist() {
