8c7ffbadef68a0a10eed96fac579138e042b2f92
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-04-08 14:46:44 +0500

    show  nav toggle btn for authenticated

--- resources/js/app/App.vue
+++ resources/js/app/App.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="row bg-dark position-relative">
             <b-navbar toggleable="sm" class="col-8 pl-4" :type="NAV_TYPE" :variant="NAV_VARIANT">
-                <b-navbar-toggle target="mainNavBar"></b-navbar-toggle>
+                <b-navbar-toggle target="mainNavBar" v-if="AUTHENTICATED"></b-navbar-toggle>
                 <b-navbar-brand :to="{name: 'home'}" class="py-0" style="width: 128px">
                     <img src="/icons/png/logo-sm.png" alt="echo.tj logo" class="w-100">
                 </b-navbar-brand>
