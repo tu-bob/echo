0ac8896c84ac019aefc79517ab695fd21906d83c
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-04-08 14:08:04 +0500

    title update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Echo.tj') }}</title>
+    <title>Echo.tj - музыкальное наследие Таджикистана</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
