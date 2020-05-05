3a83eae252efc3b95a2687db4281f8d86b55c424
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-05-05 22:03:51 +0500

    hide scrollbar for mobile + viewport size mixin

---
+++ resources/js/components/common/mixins/ViewportSize.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "viewportSize",
+        mounted() {
+            window.addEventListener('resize', () => {
+                this.windowHeight = window.innerHeight;
+                this.windowWidth = window.innerWidth;
+            });
+        },
+        data() {
+            return {
+                windowHeight: window.innerHeight,
+                windowWidth: window.innerWidth,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
