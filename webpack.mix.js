535b2d260c7fba7994ff322527217469c55ceb63
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-05-24 13:32:10 +0500

    chunks versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.config.webpackConfig.output = {
+    chunkFilename: 'scripts/[name].[chunkhash].js',
+    publicPath: '/',
+};
+
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
     .version();
