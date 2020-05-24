3dbb7ff57873b097564fda85635133804d8b1c78
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-05-24 12:25:52 +0500

    hide MusicAlbumType.php

--- Modules/Media/Models/Music/MusicAlbumType.php
+++ Modules/Media/Models/Music/MusicAlbumType.php
@@ -8,5 +8,12 @@ use Modules\Shared\Models\BaseModel;
 
 class MusicAlbumType extends BaseModel
 {
-
+    protected $hidden = [
+        'created_at',
+        'created_by_id',
+        'deleted_at',
+        'deleted_by_id',
+        'updated_at',
+        'updated_by_id'
+    ];
 }
