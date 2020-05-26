840f20b0310639bb54ea22011316fb060ecf57aa
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-05-27 02:39:18 +0500

    show song label

--- resources/js/components/public/music/SongView.vue
+++ resources/js/components/public/music/SongView.vue
@@ -25,6 +25,9 @@
                 </h2>
                 <h3 class="typography-footnote-emphasized text-uppercase text-secondary">
                     {{genres}} <span v-if="song.year">· {{song.year}}</span></h3>
+                <h3 class="typography-footnote-emphasized text-uppercase text-secondary" v-if="song.label">
+                   Лейбл {{song.label}}
+                </h3>
                 <b-button variant="outline-secondary" pill class="mt-3" @click="playSongs([song])">
                     <font-awesome-icon icon="play" size="sm" class="mr-1"></font-awesome-icon>
                     Слушать
