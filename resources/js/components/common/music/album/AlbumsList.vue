0ef4bd4a237fabf7ea879da48a84bfeb8b039b2e
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-06-01 16:02:51 +0500

    play

--- resources/js/components/common/music/album/AlbumsList.vue
+++ resources/js/components/common/music/album/AlbumsList.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <div class="row" style="padding-bottom: 10rem">
+    <div style="padding-bottom: 10rem">
+        <div class="row">
             <album-card class="col-12  col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-5"
                         v-for="album in albums" :key="album.id"
                         :album="album"></album-card>
