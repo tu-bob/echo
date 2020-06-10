86e15ffe09b0f39998159ec8b1223364edce8074
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-06-10 23:39:56 +0500

    header styling

--- resources/js/components/public/blog/PostReader.vue
+++ resources/js/components/public/blog/PostReader.vue
@@ -1,30 +1,32 @@
 <template>
     <div class="pb-5 mb-5 w-100">
         <main class="container-fluid pt-5 article" v-if="post">
-
             <b-row>
                 <img class="preview-image" :src="previewImageSrc" alt="">
             </b-row>
+            <b-row style="margin-top:3rem">
+                <b-container fluid="md">
+                    <article class="text-light-grey">
+                        <section>
+                            <div class="mb-4">
+                                <h1 class="typography-title-emphasized text-white mb-3" style="font-size:38px">{{post.title}}</h1>
+                                <div class="mb-3">
+                                    <span class="typography-title text-violet">Автор: {{post.author.name}}</span>
+                                </div>
+                                <h2 class="text-muted text-justify">{{post.annotation}}</h2>
+                            </div>
 
-            <article class="text-light-grey">
-                <section>
-                    <div class="mb-4">
-                        <h1 class="text-light mb-3">{{post.title}}</h1>
-                        <div class="mb-3">
-                            <span>Автор: {{post.author.name}}</span>
-                        </div>
-                        <h2 class="text-muted text-justify">{{post.annotation}}</h2>
+                            <div class="text-justify article-body" v-html="post.article"></div>
+                        </section>
+                    </article>
+                    <div v-if="post.reference" class="pt-4">
+                        <a :href="post.reference">
+                            <span v-if="post.ref_name">{{post.ref_name}}</span>
+                            <span v-else>{{post.reference}}</span>
+                        </a>
                     </div>
-
-                    <div class="text-justify article-body" v-html="post.article"></div>
-                </section>
-            </article>
-            <div v-if="post.reference" class="pt-4">
-                <a :href="post.reference">
-                    <span v-if="post.ref_name">{{post.ref_name}}</span>
-                    <span v-else>{{post.reference}}</span>
-                </a>
-            </div>
+                </b-container>
+            </b-row>
         </main>
     </div>
 </template>
@@ -72,7 +74,7 @@
 
     .preview-image {
         width: 100% !important;
-        height:400px !important;
+        max-height: 350px !important;
         object-fit: contain;
         vertical-align: middle;
     }
