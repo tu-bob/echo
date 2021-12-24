84a56d5320702a1545fb5194211bb7538fa8c02d
loiq.amonbekov <amonbekov.loiq@gmail.com>
2021-12-24 15:56:07 +0500

    ADMIN UI UPDATE

---
+++ resources/js/admin_app/pages/song/SongTabLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <section-header>Песни</section-header>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import SectionHeader from "../../components/SectionHeader";
+export default {
+    name: "SongTabLayout",
+    components: {SectionHeader}
+}
+</script>
+
+<style scoped>
+
+</style>
